983815e5c590479898b303414544fa9bdefd0849
Dyanna Tolman <dyannast@gmail.com>
2018-08-28 09:47:27 -0700

    fixed errors

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 
 export default {
-    getNews(searchTerm = '') {
-      return fetch(`https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(searchTerm)}&apiKey=ac3f6dc2e91b4903b93888ac7270f98f`)
-        .then(response => response.json());
-    }
-  };
+  getNews(searchTerm = '') {
+    return fetch(`https://newsapi.org/v2/top-headlines?q=${encodeURIComponent(searchTerm)}&apiKey=ac3f6dc2e91b4903b93888ac7270f98f`)
+      .then(response => response.json());
+  }
+};
 
